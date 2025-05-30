43d1c032d5401a56df3cc16ffb5e9c589ff428bf
ciorny04 <bartulablazej24@gmail.com>
2025-05-30 20:25:15 +0200

    Rozwiązanie zadania 6
    Multiply three numbers

---
+++ Tasks/Program6.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("wprowadź pierwszy numer: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("wprowadź drugi numer: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("wprowadź trzeci numer: ");
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("{0} x {1} x {2} = {3}", a, b, c, a * b * c);
